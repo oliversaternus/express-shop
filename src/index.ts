1b08f9935b37d1bc5c6a92ec12d284310e68f097
oliversaternus <mail@oliversaternus.de>
2019-08-24 18:45:16 +0200

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,7 @@ import * as models from "./models";
 import * as mongo from "./mongo";
 import * as utils from "./utils";
 
-const secret: string = "wo4Bd8FktL31Ekv8sTbcl33";
-const alphabet: string = "4fPwKEjkGrBJst2MpFVZx9y5lIm6A7LDinQzgOhqaWC3obXuv0H1cNde8Y";
-const reservedDomains: string[] = ["www", "assets"];
-const allowedAssetTypes: string[] = ["jpg", "svg", "png"];
-
 const app: express.Application = express();
-
-function randomString(length: number): string {
-    let result: string = "";
-    for (let i = 0; i < length; i++) {
-        result += alphabet[Math.floor((Math.random() * 58))];
-    }
-    return result;
-}
-
-function verifyToken(token: string): string {
-    if (!token) {
-        return "";
-    }
-    const plainToken = JSON.parse(crypt.decrypt(token));
-    if (plainToken.sec !== secret) {
-        return "";
-    }
-    return plainToken.id;
-}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
--- src/utils.ts
+++ src/utils.ts
@@ -1,43 +1,22 @@
-import fs from "fs";
-import path from "path";
-import * as models from "./models";
-import * as mongo from "./mongo";
-import * as templates from "./templates";
+import * as crypt from "./crypt";
+const secret: string = "wo4Bd8FktL31Ekv8sTbcl33";
+const alphabet: string = "4fPwKEjkGrBJst2MpFVZx9y5lIm6A7LDinQzgOhqaWC3obXuv0H1cNde8Y";
 
-export const buildPath = path.join(__dirname, "..", "build");
-
-export async function build(domain: string, page: string): Promise<boolean> {
-
-    try {
-        const pageData: models.IPage = await mongo.getPage(domain, page);
-        const generator = templates.generator(pageData.template);
-        if (fs.existsSync(path.join(buildPath, domain, page + ".html"))) {
-            fs.unlinkSync(path.join(buildPath, domain, page + ".html"));
-        }
-        fs.writeFileSync(
-            path.join(buildPath, domain, page + ".html"),
-            generator(pageData));
-        return true;
-    } catch (e) {
-        return false;
+export function randomString(length: number): string {
+    let result: string = "";
+    for (let i = 0; i < length; i++) {
+        result += alphabet[Math.floor((Math.random() * 58))];
     }
-
+    return result;
 }
 
-export async function create(domain: string): Promise<boolean> {
-
-    try {
-        const page = "index";
-        const pageData: models.IPage = await mongo.getPage(domain, page);
-        const generator = templates.generator(pageData.template);
-        fs.mkdirSync(path.join(buildPath, domain));
-        fs.mkdirSync(path.join(buildPath, domain, "assets"));
-        fs.writeFileSync(
-            path.join(buildPath, domain, page + ".html"),
-            generator(pageData));
-        return true;
-    } catch (e) {
-        return false;
+export function verifyToken(token: string): string {
+    if (!token) {
+        return "";
     }
-
+    const plainToken = JSON.parse(crypt.decrypt(token));
+    if (plainToken.sec !== secret) {
+        return "";
+    }
+    return plainToken.id;
 }
